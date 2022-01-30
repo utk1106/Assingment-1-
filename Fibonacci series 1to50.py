a67859c3ed3387d9f471a92616aea9da458674a3
utk1106 <thaokarutkarsh@gmail.com>
2022-01-30 16:03:14 +0530

    Add files via upload

---
+++ Fibonacci series 1to50.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Assingment no 1
+
+# print fibonacci series from 1 to 50
+
+# In[4]:
+
+
+x=0
+y=1
+while y<50:
+    print(y)
+    x,y=y,x+y    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
